5f49bf59bbe570903cb5993b7c0b1ee254b82d01
WolfgangWalter Sauer (wowasa) <wolfgang.sauer@oeaw.ac.at>
2017-07-04 11:41:11 +0200

    modifications provided by Paolo Scarponi

--- stanbol_wrapper/src/main/java/at/ac/acdh/enrichment/stanbol/StanbolAlgorthm.java
+++ stanbol_wrapper/src/main/java/at/ac/acdh/enrichment/stanbol/StanbolAlgorthm.java
@@ -1,97 +1,109 @@
-package at.ac.acdh.enrichment.stanbol;
+package org.gcube.data.analysis.algorithms.stanbolwrapper;
 
-import java.io.IOException;
-import java.util.Properties;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
+import org.apache.commons.io.FileUtils;
 
 import org.gcube.contentmanagement.lexicalmatcher.utils.AnalysisLogger;
 import org.gcube.dataanalysis.ecoengine.configuration.AlgorithmConfiguration;
 import org.gcube.dataanalysis.ecoengine.datatypes.PrimitiveType;
-import org.gcube.dataanalysis.ecoengine.datatypes.PrimitiveTypesList;
 import org.gcube.dataanalysis.ecoengine.datatypes.StatisticalType;
 import org.gcube.dataanalysis.ecoengine.datatypes.enumtypes.PrimitiveTypes;
 import org.gcube.dataanalysis.ecoengine.interfaces.StandardLocalExternalAlgorithm;
 
-public class StanbolAlgorthm extends StandardLocalExternalAlgorithm{
+
+public class StanbolWrapper extends StandardLocalExternalAlgorithm {
+
+  /** FIELDS **/	
 	
-	private String paramName = "input";
-	
-	/* should read from configuration */
+	/* Input parameters */
+
+	private String inputName = "input";
 	private String acceptType;
 	private String contentType;
 	private String stanbolRestUrl;
 	
-	private String result;
+	/* output parameters */
 	
+	private String outputName = "output";
+	private File outputJsonFile;
+
+  /** METHODS **/		
+	
+	@Override
+	public void init() throws Exception {
+		AnalysisLogger.setLogger(config.getConfigPath() + AlgorithmConfiguration.defaultLoggerFile);
+		AnalysisLogger.getLogger().debug("Process initialized");
+	}
 
 	@Override
 	public String getDescription() {
-		return "Accepts list of strings and tries to enrich each of them using Apache Stanbol API";
-	}
-
-	@Override
-	public void init() throws Exception {
-		// Taken from HelloWorldAlgorithm
-		AnalysisLogger.setLogger(config.getConfigPath() + AlgorithmConfiguration.defaultLoggerFile);
-		AnalysisLogger.getLogger().debug("StanbolAlgorthm process initialized");
-		
-		Properties props = new Properties();
-		try{
-			props.load(StanbolAlgorthm.class.getResourceAsStream("/stanbol_algorithm.cfg.properties"));
-			
-			acceptType = props.getProperty("acceptType");
-			contentType = props.getProperty("contentType");
-			stanbolRestUrl = props.getProperty("stanbolRestUrl");
-		}catch(IOException io){
-			//throw new Exception("Unable to locate stanbol_algorithm.cfg.properties", io);
-			
-			//defaults as backup
-			acceptType = "application/json";
-			contentType = "text/plain";
-			stanbolRestUrl = "http://enrich.acdh.oeaw.ac.at/enhancer";
-		}
-	}
-
-	@Override
-	protected void process() throws Exception {
-		String input = getInputParameter(paramName);
-		AnalysisLogger.getLogger().debug("Processing input: " + input);
-		
-		Client client = ClientBuilder.newClient();
-		WebTarget stanbol = client.target(stanbolRestUrl);		
-		
-		Response response = stanbol.request(acceptType).post(Entity.entity(input, contentType));
-		
-//		ClientConfig config = new DefaultClientConfig();
-//		Client client = Client.create(config);
-//		WebResource stanbol = client.resource(stanbolRestUrl);
-//		ClientResponse response = stanbol.accept(acceptType).type(contentType).post(ClientResponse.class, input);
-		
-		result = response.readEntity(String.class);
-		
-		AnalysisLogger.getLogger().debug("Result: " + result);
-	}
-	
-	@Override
-	public StatisticalType getOutput() {		
-		return new PrimitiveType(String.class.getName(), result, PrimitiveTypes.STRING, "result", "enriched result");
+		return "This process sends an input text file to the Apache Stanbol REST API and returns a semantically enriched output text file in json-ld format";
 	}
 
 	@Override
 	protected void setInputParameters() {		
-		inputs.add(new PrimitiveTypesList(String.class.getName(), PrimitiveTypes.STRING, paramName, "text to be enriched", false) );
+		inputs.add(new PrimitiveType(File.class.getName(), null, PrimitiveTypes.FILE, inputName, "File containing the text to be enriched", "input_text") );
+	}
+
+	@Override
+	protected void process() throws Exception {
 		
+		// This properties acquisition part has to be changed once a service endpoint has been created
+		// (the three properties will be read from specific fields inside the resource definition)
+		acceptType = "application/json";
+		contentType = "text/plain";
+		stanbolRestUrl = "http://enrich.acdh.oeaw.ac.at/enhancer";
+		
+		// This is the part in which the stanbol service is called and the response is saved into a file
+		// (the file will be available at the end of the computation inside a specific folder in the workspace)
+	    HttpURLConnection connection = null;
+		URL url = new URL(stanbolRestUrl);
+	    connection = (HttpURLConnection) url.openConnection();
+	    connection.setRequestMethod("POST");
+	    connection.setRequestProperty("Accept", acceptType);
+	    connection.setRequestProperty("Content-Type", contentType);
+	    connection.setUseCaches(false);
+	    connection.setDoOutput(true);
+	    File inputText = new File(getInputParameter(inputName));
+		BufferedReader reader = new BufferedReader(new FileReader(inputText));
+		String text = "";
+	    String line;
+		while ((line = reader.readLine()) != null)  
+			text += line;
+	    reader.close();
+		DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+		outputStream.writeBytes(text);
+	    outputStream.flush();
+	    outputStream.close();
+	    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		StringBuffer response = new StringBuffer();
+	    while ((line = reader.readLine()) != null) {
+			response.append(line);
+			response.append("\n");
+	    }
+	    reader.close();
+	    connection.disconnect();
+	    String result = response.toString();
+	    outputJsonFile = new File (outputName + ".json");
+		FileUtils.writeStringToFile(outputJsonFile, result);
+	    
+	}
+	
+	@Override
+	public StatisticalType getOutput() {		
+		return new PrimitiveType(File.class.getName(), outputJsonFile, PrimitiveTypes.FILE, outputName, "Enriched strings");
 	}
 
 	@Override
 	public void shutdown() {
-		AnalysisLogger.getLogger().debug("StanbolAlgorthm process terminated");		
+		AnalysisLogger.getLogger().debug("Process terminated");		
 	}
-	
-
+   
 }
--- stanbol_wrapper/src/test/java/at/ac/acdh/test/TestStanbol.java
+++ stanbol_wrapper/src/test/java/at/ac/acdh/test/TestStanbol.java
@@ -1,21 +1,14 @@
-package at.ac.acdh.test;
+package org.gcube.data.analysis.algorithms.stanbolwrapper;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 
 import org.gcube.dataanalysis.ecoengine.configuration.AlgorithmConfiguration;
-import org.gcube.dataanalysis.ecoengine.datatypes.PrimitiveType;
 import org.gcube.dataanalysis.ecoengine.interfaces.ComputationalAgent;
 import org.gcube.dataanalysis.ecoengine.processing.factories.TransducerersFactory;
 import org.gcube.dataanalysis.ecoengine.test.regression.Regressor;
 import org.junit.Test;
 
-import at.ac.acdh.enrichment.stanbol.StanbolAlgorthm;
-
-public class TestStanbol {
+public class StanbolWrapperTest {
 	
 	@Test
 	public void deploymentTest() throws Exception{
@@ -25,47 +18,13 @@ public class TestStanbol {
 		Regressor.process(kStatistics.get(0));
 		kStatistics.get(0).shutdown();
 		System.out.println("Deployment Test End");
-		
-		StanbolAlgorthm sa = (StanbolAlgorthm) kStatistics.get(0);
-		PrimitiveType result = (PrimitiveType) sa.getOutput();
-		
-		System.out.println("Result: \n");
-		System.out.println(result.getContent().toString());
 	}
-	
 	
 	private static AlgorithmConfiguration deploymentTestConfiguration () {
 		AlgorithmConfiguration testConfiguration = Regressor.getConfig();
-		testConfiguration.setAgent("STANBOL_ALGORITHM");
-		testConfiguration.setParam("input", "The Stanbol enhancer can detect famous cities such as Paris and people such as Bob Marley");
+		testConfiguration.setAgent("STANBOL_WRAPPER");		
+		testConfiguration.setParam("input", "data/input_text.txt");
 		return testConfiguration;	
 	}
 	
-	
-	private static AlgorithmConfiguration readInputFromFile() {
-		AlgorithmConfiguration testConfiguration = Regressor.getConfig();
-		testConfiguration.setAgent("STANBOL_ALGORITHM");
-		
-		InputStream is = TestStanbol.class.getResourceAsStream("/input_text.txt");
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		StringBuilder sb = new StringBuilder();
-		String line;
-		try {
-			while((line = br.readLine()) != null){
-				sb.append(line);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally{
-			try {
-				br.close();
-			} catch (IOException e) {
-			}
-		}		
-		
-		testConfiguration.setParam("input", sb.toString());
-		return testConfiguration;
-	}
-
 }
